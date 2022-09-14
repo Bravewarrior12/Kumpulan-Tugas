8e6503c2a9ed6a0b1150093c5c6527bceff9c59b
DELL <DELL@DESKTOP-UPAKK8J>
2022-09-14 22:05:44 +0700

    TESTING MARSA

---
+++ src/Konstruktor/M1PRAK.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Konstruktor;
+
+/**
+ *
+ * @author DELL
+ */
+class pekerja  {
+        pekerja (int umur, String gender, String jabatan){
+            System.out.println("Umur = " + umur + "gender = " + gender +" jabatan = " +jabatan);
+    }
+}
+        
+public class M1PRAK {
+    
+    public static void main(String[] args) {
+        pekerja riyan = new pekerja (31, "non-binary", "Direkktur");
+        
+    }
+    
+}
